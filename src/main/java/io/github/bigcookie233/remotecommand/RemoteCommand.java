a7f0fe9b19fd8ac5fba23ff7b85f21a350ff712a
BigCookie233 <86756026+BigCookie233@users.noreply.github.com>
2024-04-04 11:11:44 +0800

    Initial commit

---
+++ src/main/java/io/github/bigcookie233/remotecommand/RemoteCommand.java
@@ -0,0 +1,9 @@
+package io.github.bigcookie233.remotecommand;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RemoteCommand extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+}
