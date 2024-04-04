ce1ab3b54f617de36d590ce1a8deb7d700c607cb
BigCookie233 <86756026+BigCookie233@users.noreply.github.com>
2024-04-04 19:05:11 +0800

    feat: Add a config file

--- src/main/java/io/github/bigcookie233/remotecommand/RemoteCommand.java
+++ src/main/java/io/github/bigcookie233/remotecommand/RemoteCommand.java
@@ -1,9 +1,14 @@
 package io.github.bigcookie233.remotecommand;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RemoteCommand extends JavaPlugin {
+    public FileConfiguration config;
+
     @Override
     public void onEnable() {
+        this.saveDefaultConfig();
+        this.config = this.getConfig();
     }
 }
